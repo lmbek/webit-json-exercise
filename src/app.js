72e4e6c8e0eef3d8775e94ffcac962ef1b96b84e
jonathan johansen <63632617+luffe141@users.noreply.github.com>
2023-08-29 13:46:24 +0200

    app.js: modified

--- src/app.js
+++ src/app.js
@@ -4,12 +4,9 @@ import {Card} from "./league-site/Card";
 import {CardContainer} from "./league-site/CardContainer";
 import json from "../assets/json/CharacterData.json";
 import { DOM } from "./utility/DOM";
+import { Person } from "./league-site/Person";
 
 
-
-
-
-    
 
 
 const data = json.content;
@@ -18,5 +15,13 @@ const data = json.content;
 const header= document.createElement("header");
 header.id = "header";
 document.body.appendChild(header);
+header.innerText = "League of Legends";
 
-new CardContainer(data);
+const container = document.createElement("section");
+container.id = "person-container";
+document.body.appendChild(container);
+
+new Person(data[0], container);
+new Person(data[1], container);
+new CardContainer(data[0]);
+new CardContainer(data[1]);
