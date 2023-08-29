2d441695e509f3899a4610cc349a5557a7f1edf5
jonathan johansen <63632617+luffe141@users.noreply.github.com>
2023-08-29 13:47:12 +0200

    ChampionCard.js: modified

--- src/league-site/ChampionCard.js
+++ src/league-site/ChampionCard.js
@@ -1,8 +1,37 @@
 import {Card} from "./Card";
+import {DOM} from "../utility/DOM";
 
-export class ChampionCard extends Card {
+export class ChampionCard extends DOM {
     constructor(data, DOM) {
-        super(data, DOM);
+        super();
+        this.data = data;
+        // add HTML
+        this.DOM = document.createElement("div");
+        this.DOM.id = data.name;
+        DOM.appendChild(this.DOM);
+        console.log(this.DOM);
+        // add CSS
+        this.DOM.style.backgroundImage ="url("+data.img+")";
+        this.DOM.style.height ="500px";
+
+        this.addHeadline();
+        this.addParagraf();
+        
+    }
+    addHeadline (){
+        const headline = this.addElement(this.DOM,"h1");
+        headline.innerText = this.data.name;
+        headline.style.color = "white";
+        headline.style.textAlign = "center";
+        headline.style.fontSize =" 2.5rem";
     }
 
+    addParagraf(){
+        const paragraf = this.addElement(this.DOM, "p");
+        paragraf.innerText = this.data.text;
+        paragraf.style.color =" white";
+        paragraf.style.textAlign = "center";
+        paragraf.style.fontSize =" 2.5rem";
+
+    }
 }
