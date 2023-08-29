2a2e9d6b6bb84e02f2023cf670f60d348522b7d1
jonathan johansen <63632617+luffe141@users.noreply.github.com>
2023-08-29 13:47:24 +0200

    Person.js: modified

--- src/league-site/Person.js
+++ src/league-site/Person.js
@@ -1,5 +1,18 @@
 import {DOM} from "../utility/DOM";
 
-class Person extends DOM {
+export class Person extends DOM {
 
+    constructor(data, container){
+        super();
+
+        // add the person DOM
+        this.DOM = this.addElement(container, "div");
+
+        this.data = data;
+
+        this.DOM.id = data.name
+
+
+
+    }
 }
